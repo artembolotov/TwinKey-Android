9afb971bf81883b96b61920754a94eaff5d62332
Artem Bolotov <teeemon@gmail.com>
2026-04-19 15:29:04 +0300

    AddManuallyScreen.kt fixed keyboard

--- app/src/main/java/com/artembolotov/twinkey/ui/add/AddManuallyScreen.kt
+++ app/src/main/java/com/artembolotov/twinkey/ui/add/AddManuallyScreen.kt
@@ -27,6 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -69,12 +72,21 @@ fun AddManuallyScreen(
         }
     }
     val canDone by remember { derivedStateOf { issuer.isNotBlank() && secretValid } }
+    val focusManager = LocalFocusManager.current
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
-            .padding(16.dp),
+            .padding(16.dp)
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        awaitPointerEvent(PointerEventPass.Initial)
+                        focusManager.clearFocus()
+                    }
+                }
+            },
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
